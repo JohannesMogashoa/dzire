d1fe6b58242c5aebc3e71ef7bbc4bfd2f86da6ad
Johannes Mogashoa <jomogashoa1993@gmail.com>
2025-06-01 18:40:21 +0200

    Initialize project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Home } from './home/home';
 import { Routes } from '@angular/router';
+import { Wishlist } from './wishlist/wishlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'wishlist',
+    component: Wishlist,
+  },
+];
